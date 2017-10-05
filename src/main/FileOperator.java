572064bde11c9eca6dc8a22f03b55e884cfb88af
Xanible <cbp1222@jagmail.southalabama.edu>
2017-10-05 09:47:53 -0500

    First Steps

---
+++ src/main/FileOperator.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class FileOperator {
+	
+	//Variables
+	File sourceDir;
+	File outputDir;
+	File[] sourceFiles;
+	File[] outputFiles;
+	
+	//Constructor
+	public FileOperator(String sourceBaseDir, String outputBaseDir){
+		sourceDir = new File(sourceBaseDir);
+		outputDir = new File(outputBaseDir);
+		sourceFiles = sourceDir.listFiles();
+		outputFiles = outputDir.listFiles();
+	}
+	
+	//Method
+	public void moveFileToOutput(File f) {
+		try {
+			File output = new File(outputDir.toPath() + File.separator + f.getName());
+			Files.move(f.toPath(), output.toPath());
+		} catch (IOException e) {
+			System.out.println("File " + f.getName() + " could not be moved");
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.io.File;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//TEST
+		FileOperator FO = new FileOperator("C:\\Users\\colby\\Desktop\\TEST\\SOURCE",
+				"C:\\Users\\colby\\Desktop\\TEST\\OUTPUT");
+		
+		
+	}
+	
+	public static void sortFiles() {
+		//Create FileOperator
+		
+		//Read in list of file names
+		
+		//Split names based on destination
+		
+		//Start sorting loop
+		
+			//
+	}
+
+}
